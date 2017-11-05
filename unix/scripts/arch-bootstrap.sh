e89c57890d7a29b5d4dec42dfa9218d2b246f7e4
Victor Torres <vpaivatorres@gmail.com>
2017-11-05 09:39:04 -0300

    add arch linux boostrap script

---
+++ unix/scripts/arch-bootstrap.sh
@@ -0,0 +1,16 @@
+# Pacman packages
+sudo pacman -Sy gnome-tweak-tool
+sudo pacman -Sy python-pip
+sudo pacman -Sy postgresql
+sudo pacman -Sy libmariadbclient
+sudo pacman -Sy swig
+sudo pacman -Sy rsync
+
+# Yaourt packages
+yaourt -S arc-gtk-theme
+yaourt -S slack-desktop
+yaourt -S google-chrome
+
+# Python packages
+sudo pip install virtualenvwrapper
+sudo pip install wakatime
