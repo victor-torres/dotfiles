2a77f769ccf86db8b9ae57f7da774a22396aa10e
Victor Torres <vpaivatorres@gmail.com>
2017-12-27 18:31:00 -0300

    add archlinux.fr repo and DESKTOP flag

--- unix/scripts/arch-bootstrap.sh
+++ unix/scripts/arch-bootstrap.sh
@@ -1,20 +1,36 @@
-# Pacman packages
-sudo pacman -Sy gnome-tweak-tool
-sudo pacman -Sy python-pip
-sudo pacman -Sy postgresql
-sudo pacman -Sy libmariadbclient
-sudo pacman -Sy swig
-sudo pacman -Sy rsync
-sudo pacman -Sy exfat-utils
-sudo pacman -Sy transmission-gtk
+# archlinux.fr
+if [ -n "grep archlinuxfr /etc/pacman.conf" ] ; then
+    sudo sh -c "cat <<EOF >> /etc/pacman.conf
+[archlinuxfr]
+SigLevel = Never
+Server = http://repo.archlinux.fr/x86_64
+EOF"
+fi
 
-# Yaourt packages
-yaourt -S arc-gtk-theme
-yaourt -S slack-desktop
-yaourt -S google-chrome
-yaourt -S chrome-gnome-shell-git
+# Pacman packages
+sudo pacman -Sy yaourt
+sudo pacman -S python-pip
+sudo pacman -S postgresql
+sudo pacman -S libmariadbclient
+sudo pacman -S swig
+sudo pacman -S rsync
+sudo pacman -S exfat-utils
 
 # Python packages
 sudo pip install virtualenvwrapper
 sudo pip install wakatime
 sudo pip install mercurial
+
+# Desktop
+if [ $DESKTOP ] ; then
+    # Pacman packages
+    sudo pacman -S gnome-tweak-tool
+    sudo pacman -S transmission-gtk
+
+    # Yaourt packages
+    yaourt -S arc-gtk-theme
+    yaourt -S slack-desktop
+    yaourt -S google-chrome
+    yaourt -S chrome-gnome-shell-git
+
+fi
