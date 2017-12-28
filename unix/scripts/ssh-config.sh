1a15d1ae25bdbb2b7e87d79a46e28137824bfd2a
Victor Torres <vpaivatorres@gmail.com>
2017-12-28 12:26:17 -0300

    rename ssh last pass notes

--- unix/scripts/ssh-config.sh
+++ unix/scripts/ssh-config.sh
@@ -2,7 +2,7 @@
 mkdir -p ~/.ssh
 sudo mkdir -p /root/.ssh
 
-lpass show --name "SSH Private Key" --notes > ~/.ssh/id_rsa
-lpass show --name "SSH Public Key" --notes > ~/.ssh/id_rsa.pub
+lpass show --name id_rsa --notes > ~/.ssh/id_rsa
+lpass show --name id_rsa.pub --notes > ~/.ssh/id_rsa.pub
 
 cat ~/.ssh/id_rsa.pub | sudo tee /root/.ssh/authorized_keys
