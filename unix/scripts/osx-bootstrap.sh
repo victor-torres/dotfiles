ceb560e12504b99dc6e663e0857f61101f603116
Victor Torres <vpaivatorres@gmail.com>
2018-01-19 14:29:53 -0300

    install command-line-tweeter in mac os x

--- unix/scripts/osx-bootstrap.sh
+++ unix/scripts/osx-bootstrap.sh
@@ -6,3 +6,6 @@ brew install ledger
 brew install rsync
 brew install tmux
 brew install tmate
+
+wget https://github.com/vmchale/command-line-tweeter/releases/download/1.0.1.32/tweet-x86_64-apple-darwin -O /usr/local/bin/command-line-tweeter
+sudo chmod +x /usr/local/bin/command-line-tweeter
