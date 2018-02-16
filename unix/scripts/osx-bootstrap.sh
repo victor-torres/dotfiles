6342e7151a216aeaebc9d6d09423b1da825fa77c
Victor Torres <vpaivatorres@gmail.com>
2018-02-16 11:13:06 -0300

    mercurial merge tool p4merge

--- unix/scripts/osx-bootstrap.sh
+++ unix/scripts/osx-bootstrap.sh
@@ -7,5 +7,10 @@ brew install rsync
 brew install tmux
 brew install tmate
 
+brew cask install p4merge
+
 wget https://github.com/vmchale/command-line-tweeter/releases/download/1.0.1.32/tweet-x86_64-apple-darwin -O /usr/local/bin/command-line-tweeter
 sudo chmod +x /usr/local/bin/command-line-tweeter
+
+echo "/Applications/p4merge.app/Contents/MacOS/p4merge ${@}" > /usr/local/bin/p4merge
+chmod +x /usr/local/bin/p4merge
