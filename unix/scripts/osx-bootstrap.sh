eb233bd57cfd826919b3a67f0f4385887a498814
Victor Torres <vpaivatorres@gmail.com>
2019-01-25 15:39:07 -0300

    configure p4merge on macos

--- unix/scripts/osx-bootstrap.sh
+++ unix/scripts/osx-bootstrap.sh
@@ -12,5 +12,5 @@ brew cask install p4merge
 wget https://github.com/vmchale/command-line-tweeter/releases/download/1.0.1.32/tweet-x86_64-apple-darwin -O /usr/local/bin/command-line-tweeter
 sudo chmod +x /usr/local/bin/command-line-tweeter
 
-echo "/Applications/p4merge.app/Contents/MacOS/p4merge ${@}" > /usr/local/bin/p4merge
+ln -sf /Applications/p4merge.app/Contents/Resources/launchp4merge /usr/local/bin/p4merge
 chmod +x /usr/local/bin/p4merge
